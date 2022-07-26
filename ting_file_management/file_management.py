8b45bd8dea34c768db475a18864a7d5e90571503
Aline <eualine2017@gmail.com>
2022-07-26 14:48:39 -0300

    Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith('.txt'):
+        try:
+            arquivo = open(path_file)
+            return arquivo.splitlines()
+        except FileNotFoundError:
+            sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+    else:
+        sys.stderr.write('Formato inválido\n')
